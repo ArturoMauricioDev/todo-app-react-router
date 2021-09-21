3a0bafeecaca4ccea53af8d6c53fed95b01af204
Arturo <atercerosb@univalle.edu>
2021-09-21 10:12:09 -0400

    utilizando useContext

--- src/AppUI.js
+++ src/AppUI.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TodoContext } from './context/TodoContext';
 import { TodoCounter } from './components/TodoCounter'
 import { TodoSearch } from './components/TodoSearch'
@@ -7,23 +7,23 @@ import { TodoItem } from './components/TodoItem'
 import { CreateTodoButton } from './components/CreateTodoButton'
 
 function AppUI() {
+
+    const { filteredText,
+        toggleTodo,
+        deleteTodo } = useContext(TodoContext)
+
     return (
         <>
             <TodoCounter />
             <TodoSearch />
-            <TodoContext.Consumer>
-                {({ filteredText,
-                    toggleTodo,
-                    deleteTodo }) => {
-                    return (
-                        <TodoList>
-                            {filteredText.map(todo => (
-                                <TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => toggleTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} />
-                            ))}
-                        </TodoList>
-                    )
-                }}
-            </TodoContext.Consumer>
+
+            <TodoList>
+                {filteredText.map(todo => (
+                    <TodoItem key={todo.text} text={todo.text} completed={todo.completed} onComplete={() => toggleTodo(todo.text)} onDelete={() => deleteTodo(todo.text)} />
+                ))}
+            </TodoList>
+            )
+
             <CreateTodoButton />
         </>
     )
--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { TodoContext } from '../context/TodoContext'
 
-function TodoCounter({ total, completed }) {
+function TodoCounter() {
+    const { totalTodos, completedTodos } = useContext(TodoContext)
     return (
         <h2 className="todo-counter">
-            Has completado {completed} de {total} TODOs
+            Has completado {completedTodos} de {totalTodos} TODOs
         </h2>
     )
 }
